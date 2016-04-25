eb7e68e7ed1abee11d8753f9ddbbcd57ab22d6c9
Mike Lambert <mlambert@gmail.com>
2016-04-25 18:49:02 +0900

    Initial version that rlies on recent changes to react-native-fabric to send React Native JS exceptions to the Crashlytics service.

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import StackTrace from 'stacktrace-js';
+import { Crashlytics } from 'react-native-fabric';
+
+function init() {
+  var originalHandler = global.ErrorUtils.getGlobalHandler();
+  function errorHandler(e) {
+    StackTrace.fromError(e).then((x)=>Crashlytics.recordCustomExceptionName(e.message, e.message, x));
+    // And then re-throw the exception with the original handler
+    originalHandler(e);
+  }
+  global.ErrorUtils.setGlobalHandler(errorHandler);
+}
+
+module.exports = {
+  init,
+}
