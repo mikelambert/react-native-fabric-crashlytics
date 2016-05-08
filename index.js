6585e4254ebe6ec9deeb0643139c9ee9f770a57e
Mike Lambert <mlambert@gmail.com>
2016-05-07 20:54:54 -0400

    Don't send crashlytics errors to the server in dev mode.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import StackTrace from 'stacktrace-js';
 import { Crashlytics } from 'react-native-fabric';
 
 function init() {
+  if (__DEV__) {
+    // Don't send exceptions from __DEV__, it's way too noisy!
+    // Live reloading and hot reloading in particular lead to tons of noise...
+    return;
+  }
+
   var originalHandler = global.ErrorUtils.getGlobalHandler();
   function errorHandler(e) {
     StackTrace.fromError(e).then((x)=>Crashlytics.recordCustomExceptionName(e.message, e.message, x));
