14de8bbd5c40493680cbd68f3983465f0ddaa1e1
Matt Lanter <matt@opendoor.com>
2016-12-15 17:45:07 -0800

    [android] Don't group crash reports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { Platform } from 'react-native';
 import StackTrace from 'stacktrace-js';
 import { Crashlytics } from 'react-native-fabric';
 
@@ -20,7 +21,18 @@ function init() {
     });
     // And then re-throw the exception with the original handler
     if (originalHandler) {
-      originalHandler(e, isFatal);
+      if (Platform.OS === 'ios') {
+        originalHandler(e, isFatal);
+      } else {
+        // On Android, throwing the original exception immediately results in the
+        // recordCustomExceptionName() not finishing before the app crashes and therefore not logged
+        // Add a delay to give it time to log the custom JS exception before crashing the app.
+        // The user facing effect of this delay is that separate JS errors will appear as separate
+        // issues in the Crashlytics dashboard.
+        setTimeout(() => {
+          originalHandler(e, isFatal);
+        }, 500);
+      }
     }
   }
   global.ErrorUtils.setGlobalHandler(errorHandler);
